f8df76c6832738e4002df18254d208d3cf76b5c3
Sergio Pomárico <sergiodavid21@gmail.com>
2023-08-05 16:32:29 -0500

    chore: :wrench: add and config tailwind CSS on astro

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      theme: {
+        colors: {
+          darkblue: {
+            100: '#2237D1',
+            200: '#182691',
+            300: '#090F37',
+          },
+        },
+        fontFamily: {
+          vietnam_pro: ['Be Vietnam Pro'],
+        },
+      },
+    },
+  },
+  plugins: [],
+};
