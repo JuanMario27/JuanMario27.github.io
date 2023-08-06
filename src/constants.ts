91330f7a07b2a2ccd5f80c42a390388e44e77cf5
Sergio Pomárico <sergiodavid21@gmail.com>
2023-08-06 07:18:45 -0500

    feat: :sparkles: add head tag component

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'intest | integrando estructuras';
+export const SITE_DESCRIPTION =
+  'Trabajamos en el desarrollo de estructuras económicas, seguras y funcionales, nuestra comunicación cercana con los clientes impulsa proyectos de alto valor técnico y eficiencia garantizada.';
