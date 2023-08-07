31371d2239618427b5ccba8ad28a419aee441f9d
Sergio Pomárico <sergiodavid21@gmail.com>
2023-08-07 16:45:53 -0500

    feat: :lipstick: add style to markdown post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,32 @@
 import {defineConfig} from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import addClasses from 'rehype-add-classes';
 
 import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), image()],
+  markdown: {
+    rehypePlugins: [
+      [
+        addClasses,
+        {
+          h1: 'title1',
+          h2: 'title2',
+          h3: 'title3',
+          h4: 'title4',
+          p: 'paragraph',
+          img: 'photo',
+          ul: 'ulist',
+          ol: 'olist',
+          blockquote: 'blockquote',
+          table: 'table',
+          kbd: 'keyboard',
+          code: 'inline-code',
+        },
+      ],
+    ],
+  },
 });
