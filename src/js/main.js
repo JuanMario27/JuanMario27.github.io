a69d15b9769b2a2c0cd2770c127b1f650617e7be
Sergio Pomárico <sergiodavid21@gmail.com>
2023-09-09 18:22:30 -0500

    feat: :lipstick: add scroll animation to count up section

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,11 @@ class App {
     this.closeButton = document.getElementById('close-button');
     this.menuMobile = document.getElementById('menu-mobile');
     this.header = document.querySelector('.header');
+    this.counting = document.getElementById('counting');
   }
   init() {
     this.bindUIActions();
-    this.countingAnimation();
+    this.bindInterceptor();
   }
 
   bindUIActions() {
@@ -22,26 +23,35 @@ class App {
     });
   }
 
+  bindInterceptor() {
+    this.observer = new IntersectionObserver(this.countingAnimation, {
+      threshold: 1,
+    });
+    this.observer.observe(this.counting);
+  }
+
   toggleMenu() {
     this.menuMobile?.classList.toggle('menu-active');
   }
 
-  countingAnimation() {
-    const counters = document.querySelectorAll('.num');
-    counters.forEach(counter => {
-      const updateCounter = () => {
-        const target = parseInt(counter.getAttribute('data-val'));
-        const increment = target / 500;
-        const c = +counter.textContent;
-        if (c < target) {
-          counter.textContent = `+${Math.ceil(c + increment)}`;
-          setTimeout(updateCounter, 2);
-        } else {
-          counter.textContent = `+${target}`;
-        }
-      };
-      updateCounter();
-    });
+  countingAnimation(entries, __) {
+    if (entries[0].isIntersecting) {
+      const counters = document.querySelectorAll('.num');
+      counters.forEach(counter => {
+        const updateCounter = () => {
+          const target = parseInt(counter.getAttribute('data-val'));
+          const increment = target / 500;
+          const c = +counter.textContent;
+          if (c < target) {
+            counter.textContent = `+${Math.ceil(c + increment)}`;
+            setTimeout(updateCounter, 2);
+          } else {
+            counter.textContent = `+${target}`;
+          }
+        };
+        updateCounter();
+      });
+    }
   }
 }
 
