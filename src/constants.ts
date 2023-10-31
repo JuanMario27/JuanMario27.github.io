f440e2417f1730907a7e69180c4d705dc856f3b5
JUAN MARIO <jmguevara93@gmail.com>
2023-10-31 16:17:28 -0500

    Actualizacion de variables del contador

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,6 @@ export const SITE_TITLE = 'intest | integrando estructuras';
 export const SITE_DESCRIPTION =
   'Trabajamos en el desarrollo de estructuras económicas, seguras y funcionales, nuestra comunicación cercana con los clientes impulsa proyectos de alto valor técnico y eficiencia garantizada.';
 export const SITE_PHONE = '+573194449408';
+export const M2_ESTRUCTURA_METALICA = "500000"
+export const M2_ESTRUCTURA_CONCRETO_REFORZADO = "500000"
+export const ESTRUCTURAS_PROYECTADAS = "200"
